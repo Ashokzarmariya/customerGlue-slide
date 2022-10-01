59051cdd8f9cfd29dce152e208ddb7a5e0627aee
Ashokzarmariya <ashokzarmariya47@gmail.com>
2022-10-01 18:52:42 +0530

    black backdrop

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
-
-import { useState } from 'react';
-import BottomSheet from './Components/BottomSheet';
-import "./App.css"
+import { useState } from "react";
+import BottomSheet from "./Components/BottomSheet";
+import "./App.css";
 
 function App() {
-  const [isOpen,setOpen]=useState(false)
+  const [isOpen, setOpen] = useState(false);
+  const data = [
+    {
+      title: "Google Keep",
+      text: "Add to a note",
+    },
+    {
+      title: "Google Docs",
+      text: "Embed in a document",
+    },
+    {
+      title: "Google Plus",
+      text: "Share with your friends",
+    },
+    {
+      title: "Google Hangouts",
+      text: "Show to your coworkers",
+    },
+  ];
   return (
-    <div
-      onClick={() => {
-        console.log("clicked")
-        return isOpen ? setOpen(false) : ""
-      }}
-      className="">
-      <div className='flex justify-center py-20 '>
+    <div className="overflow-y-hidden">
+      <div className="flex justify-center py-20 ">
         <button
-          onClick={()=>setOpen(true)}
-          className='px-10 py-3 bg-[#2acdff] rounded-sm text-white font-bold'>Open Bottom Sheet</button>
+          onClick={() => setOpen(true)}
+          className="px-10 py-3 bg-[#2acdff] rounded-sm text-white font-bold"
+        >
+          Open Bottom Sheet
+        </button>
       </div>
-     
-      <div className={`sheet ${isOpen?"animatedSheet":"nonAnimatedSheet"}`}>
-        <BottomSheet data={[1,2,3,4,5,6]}/>
+
+      <div
+        className={`sheet ${
+          isOpen ? "animatedSheet" : "nonAnimatedSheet"
+        }`}
+      >
+        <BottomSheet data={data} />
       </div>
-      
+      <div
+        onClick={() => {
+          console.log("clicked");
+          return isOpen ? setOpen(false) : "";
+        }}
+        className={` ${
+          isOpen ? "block" : "hidden"
+        } bg-black z-10 h-screen w-screen absolute opacity-50 top-0`}
+      ></div>
     </div>
   );
 }
--- src/Components/BottomSheet.jsx
+++ src/Components/BottomSheet.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const BottomSheet = ({data}) => {
   return (
-    <div className='rounded-sm '>
-    <div className="shadow-lg shadow-gray-400 p-10 space-y-5 min-w-[34vw]">
+    <div className='rounded-sm'>
+    <div className="shadow-lg  p-10 space-y-5 min-w-[34vw] rounded-t-md bg-white">
      {data.map((item) => <div className=''>
-      <p>{item} title</p>
-      <p>Lorem ipsum dolor sit amet consectetur.</p>
+      <p>{item.title}</p>
+       <p>{item.text}</p>
      </div>)}
      
       </div>
