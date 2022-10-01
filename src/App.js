3a2778e513c69c27b695d5581827fafd0c75a836
Ashokzarmariya <ashokzarmariya47@gmail.com>
2022-10-01 18:26:00 +0530

    animation completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
 import { useState } from 'react';
 import BottomSheet from './Components/BottomSheet';
+import "./App.css"
 
 function App() {
   const [isOpen,setOpen]=useState(false)
   return (
     <div
-      onClick={()=> isOpen?setOpen(false):"" }
+      onClick={() => {
+        console.log("clicked")
+        return isOpen ? setOpen(false) : ""
+      }}
       className="">
       <div className='flex justify-center py-20 '>
         <button
@@ -14,12 +18,12 @@ function App() {
           className='px-10 py-3 bg-[#2acdff] rounded-sm text-white font-bold'>Open Bottom Sheet</button>
       </div>
      
-      <div className={`absolute left-[33vw] transition-all duration-75 ${isOpen? "bottom-0":"top-[-100vh]"} `}>
+      <div className={`sheet ${isOpen?"animatedSheet":"nonAnimatedSheet"}`}>
         <BottomSheet data={[1,2,3,4,5,6]}/>
       </div>
       
     </div>
   );
 }
-
+// buttomSheet
 export default App;
