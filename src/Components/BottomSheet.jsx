96c158d73a3664515e0c9c73f4d73e717edd4a77
Ashokzarmariya <ashokzarmariya47@gmail.com>
2022-10-01 19:16:21 +0530

    make responsive

--- src/Components/BottomSheet.jsx
+++ src/Components/BottomSheet.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const BottomSheet = ({data}) => {
   return (
     <div className='rounded-sm'>
-    <div className="shadow-lg  p-10 space-y-5 min-w-[34vw] rounded-t-md bg-white">
+    <div className="shadow-lg  p-10 space-y-5 w-[60vw] lg:w-[34vw] rounded-t-md bg-white">
      {data.map((item) => <div className=''>
       <p>{item.title}</p>
        <p>{item.text}</p>
